38bb32b7fa57b08a96819fed8f9ce8277bab3700
ifeoluwa <Aderojukadijat02@gmail.com>
2022-09-08 20:25:46 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
-* main - Prints the sizeof many var types
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float e;
-printf("Size of a char: %d byte(s)\n", sizeof(a));
-printf("Size of an int: %d byte(s)\n", sizeof(b));
-printf("Size of a long int: %d byte(s)\n", sizeof(c));
-printf("Size of a long long int: %d byte(s)\n", sizeof(d));
-printf("Size of a float: %d byte(s)\n", sizeof(e));
-return (0);
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
