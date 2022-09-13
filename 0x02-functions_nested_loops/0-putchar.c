ecd360c62f6d1f58fdb083539407b5de6ef70668
ifeoluwa349 <aderojukadijat02@gmail.com>
2022-09-13 07:02:43 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - prints _putchar, followed by a new line 
+ * main - prints _putchar, followed by a new line
  * Return: 0 (Success)
  */
 int main(void)
