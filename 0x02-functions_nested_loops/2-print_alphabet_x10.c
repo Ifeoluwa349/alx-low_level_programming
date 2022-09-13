5dad7205f4d7f0e0c040213043a735ef4280da9c
ifeoluwa349 <aderojukadijat02@gmail.com>
2022-09-13 10:15:14 -0700

    alphabet_x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
  * print_alphabet_x10 - prints 10 times the alphabet in lowercase,followed by a new line
  * Return: void
  */
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
 {
 char ch;
 int i;
@@ -11,8 +11,8 @@ for (i = 0; i < 10; i++)
 {
 for (ch = 'a'; ch <= 'z'; ch++)
 {
-_putchar(ch);
+putchar(ch);
 }
-_putchar('\n');
+putchar('\n');
 }
 }
