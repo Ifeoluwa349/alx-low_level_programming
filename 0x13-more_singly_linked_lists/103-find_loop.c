a67d445bb25c67cee35af277df9b2ee523bfe360
ifeoluwa <Aderojukadijat02@gmail.com>
2022-10-26 03:12:04 +0100

    find_loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -6,14 +6,13 @@
  *
  * Return: address of the node where the loop starts, or NULL
  */
+
 listint_t *find_listint_loop(listint_t *head)
 {
     listint_t *slow = head;
     listint_t *fast = head;
-
     if (!head)
         return (NULL);
-
     while (slow && fast && fast->next)
     {
         fast = fast->next->next;
